bef78f9d336f05777210098d2589dfb7d513f8bd
coldburnout <pxkh.212@gmail.com>
2025-04-13 23:52:16 -0600

    Creacion de Modelos: Producto y Ordenes junto con su relacion

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Producto, Orden
+from django.contrib.auth.models import Group
+
+admin.site.site_header = "Control de Inventario"
+
+class administradorDeProductos(admin.ModelAdmin):
+    list_display = ('nombre','categoria','cantidad',)
+    list_filter = ('categoria',)
 
 # Register your models here.
+admin.site.register(Producto, administradorDeProductos)
+admin.site.register(Orden)
+#admin.site.unregister(Group)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+CATEGORIA=(
+    ('Papeleria','Papeleria'),
+    ('Electronica','Electronica'),
+    ('Comida','Comida'),
+)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, null=True)
+    categoria = models.CharField(max_length=20, choices=CATEGORIA,null=True)
+    cantidad = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.nombre}-{self.cantidad}'
+    
+class Orden(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True)
+    personal = models.ForeignKey(User, models.CASCADE,null=True)
+    cantidad_de_orden = models.PositiveIntegerField(null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural ='Ordenes'
+
+    def __str__(self):
+        return f'{self.producto} ordenado por {self.personal.username}'
